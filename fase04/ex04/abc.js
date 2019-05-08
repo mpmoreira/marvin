e12b651f99d5527f511f2137a4b26c0bbce224b7
mpmoreira <49699181+mpmoreira@users.noreply.github.com>
2019-05-08 20:02:35 -0300

    abc.js

---
+++ fase04/ex04/abc.js
@@ -0,0 +1,10 @@
+function abc(x) {
+ if (x.substring(0,1) === "A") {return "true"} 
+ if (x.substring(0,1) === "B") {return "true"}
+ if (x.substring(0,1) === "C") {return "true"}
+ if (x.substring(0,1) === "c") {return "true"}
+ if (x.substring(0,1) === "b") {return "true"} 
+ if (x.substring(0,1) === "a") {return "true"} 
+ 
+    else {return("false")}
+}
