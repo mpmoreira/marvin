7e385ca126aa4293f26d8375e269f17fe6a4647b
mpmoreira <49699181+mpmoreira@users.noreply.github.com>
2019-05-08 19:45:57 -0300

    vogalOuConsoante.js

---
+++ fase04/ex03/vogalOuConsoante.js
@@ -0,0 +1,7 @@
+function vogalOuConsoante(letra) {
+ if (letra === "a"||letra === "e"||letra==="i"||letra==="o"||letra==="U"
+ ||letra==="u"||letra==="A"||letra==="E"||letra==="I"||letra==="O")
+ return "vogal"
+    else
+    return "consoante"
+}
