6cbbe407140866e22aeb6b4e904db88e5def6a23
mpmoreira <49699181+mpmoreira@users.noreply.github.com>
2019-05-09 19:53:29 -0300

    Create alfabetoMilitar.js

---
+++ fase04/ex08/alfabetoMilitar.js
@@ -0,0 +1,15 @@
+function alfabetoMilitar(x) {
+    if(x=== "A" || x=== "a")
+return "alpha"
+if (x==="E" || x==="e")
+return "echo"
+if (x==="I" || x==="i")
+return "india"
+if (x==="O" || x==="o")
+return "oscar"
+if (x==="U" || x==="u")
+return "uniform"
+
+else 
+return "false"
+}
