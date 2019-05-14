55b7b812244186c79a6b3add285a50746a43a9de
mpmoreira <49699181+mpmoreira@users.noreply.github.com>
2019-05-14 18:53:33 -0300

    Update acharNumero.js

--- fase04/ex07/acharNumero.js
+++ fase04/ex07/acharNumero.js
@@ -1,10 +1,8 @@
-function acharNumero(num) {
-if (num<5)
-return "a"
-if(num>4 && num<11)
-return "b"
-if(num>9 && num<101)
-return "c"
-if(num>100)
-return "d"
+    
+function acharNumero(x) {
+ if (x<5) {return("a")}
+ if (x>=5 && x<=10) {return("b")}
+ if (x>10 && x<=100) {return("c")}
+ else {return("d")}
+ 
 }
