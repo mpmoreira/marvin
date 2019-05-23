3606683c91a8b9d9855eecaa9fbdb50e00f412f4
mpmoreira <49699181+mpmoreira@users.noreply.github.com>
2019-05-23 19:12:39 -0300

    primo.js

---
+++ fase06/ex03/primo.js
@@ -0,0 +1,11 @@
+function primo(x) {
+  if (x===2 || x===3 || x===5 || x===7 || x===11)
+    return "Sim"
+
+    if (x%2===0 || x%3===0 || x%5===0 || x%7===0 || x%11===0)
+    return "Não"
+
+    else 
+        return "Sim"
+        
+}
